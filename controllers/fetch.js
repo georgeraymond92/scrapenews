851bea86a53abebca25bb02af79da6a8d25ae753
george <geomarray@gmail.com>
2018-09-03 15:53:41 -0700

    working scrape function for the la times website'

--- controllers/fetch.js
+++ controllers/fetch.js
@@ -3,12 +3,14 @@ var scrape = require('../scripts/scrape');
 
 module.exports = {
     scrapeHeadlines: function(req, res) {
+        console.log('hello');
         return scrape()
         .then(function(articles) {
             return db.Headline.create(articles);
         })
         .then(function(dbHeadline) {
             if (dbHeadline.length === 0) {
+                console.log('test');
                 res.json({
                     message: "Sorry there are no new articles today. Check back tomorrow!"
                 });
--- scripts/scrape.js
+++ scripts/scrape.js
@@ -5,52 +5,42 @@ var scrape = function() {
     request('http://www.latimes.com', function(err, res, body) {
         // console.log(body);
         var $ = cheerio.load(body);
+        var articles = [];
 
-        // console.log($('.flex-container-column').children('h5').text());
-        $('.card-content').children('.flex-container-column').each(function(i, element){
-            console.log($(this).children('a').text().trim());
-            // console.log($(this).children('a').attr('href'));
-            // console.log($(this).children('p').text().trim());
-            // console.log($(this).children('p').text().trim());
+        $('.flex-container-column').each(function(i, element){
+
+            var head = '';
+            var url = '';
+            var sum = '';
+
+            // console.log($(this).children('h5').text().trim());
+            if ($(this).children('h5').text().trim().length > 0) {
+                head = $(this).children('h5').text().trim();
+                // console.log($(this).children('h5').text().trim().length);
+                url = $(this).children('h5').children('a').attr('href');
+                if ($(this).children('p').text().trim().length > 0) {
+                    sum = $(this).children('p').text().trim();
+                }
+            }
+            if (head && sum && url) {
+                
+                var headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+                var sumNeat = sum.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+                
+
+                var dataToAdd = {
+                    headline: headNeat,
+                    summary: sumNeat,
+                    url: url
+                };
+                console.log(dataToAdd);
+
+                articles.push(dataToAdd);
+            }
+
         });
-        // var articles = [];
 
-        // $('.flex-container-column').each(function(i, element) {
-
-        //     var head = $(this)
-        //     .children('h5')
-        //     .text()
-        //     .trim();
-
-        //     var url = $(this)
-        //     .children('h5')
-        //     .children('a')
-        //     .attr('href');
-
-
-        //     var sum = $(this)
-        //     .children('p')
-        //     .text()
-        //     .trim();
-
-        //     if (head && sum && url) {
-                
-        //         var headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
-        //         var sumNeat = sum.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
-                
-
-        //         var dataToAdd = {
-        //             headline: headNeat,
-        //             summary: sumNeat,
-        //             url: url
-        //         };
-        //         console.log(dataToAdd);
-
-        //         articles.push(dataToAdd);
-        //     }
-
-        // });
-        // return articles;
+        return articles;
     });
 };
 
